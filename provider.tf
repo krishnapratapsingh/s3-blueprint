06babfad025d2dee529e175eeefdf0c1843df41f
krishnapratapsingh <42776288+krishnapratapsingh@users.noreply.github.com>
2022-02-09 13:18:11 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+    region = "us-east-1"
+    assume_role {
+        role_arn = "arn:aws:iam::601452778024:role/serverless-jenkins-ecs-execution-role"
+        profile = "default"
+    }
+}
