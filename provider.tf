fc17cc18b09bda386b9771cfd0f9fccb7594e80d
krishnapratapsingh <42776288+krishnapratapsingh@users.noreply.github.com>
2022-02-09 13:22:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,5 @@ provider "aws" {
     region = "us-east-1"
     assume_role {
         role_arn = "arn:aws:iam::601452778024:role/serverless-jenkins-ecs-execution-role"
-        profile = "default"
     }
 }
