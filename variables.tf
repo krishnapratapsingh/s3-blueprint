f858797693cef1adfd49be8b03fde9d005bf7e55
krishnapratapsingh <42776288+krishnapratapsingh@users.noreply.github.com>
2022-02-09 18:00:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-variable "bucket_prefix" {
+variable "bucket_name" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
     default     = ""
