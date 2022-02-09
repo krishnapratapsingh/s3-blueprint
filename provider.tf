9dc4ca4f42bc2bbdc2020019b7d98699e63cade4
krishnapratapsingh <42776288+krishnapratapsingh@users.noreply.github.com>
2022-02-09 13:29:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
-    assume_role {
-        role_arn = "arn:aws:iam::601452778024:role/serverless-jenkins-ecs-execution-role"
-    }
+  # Declaring AssumeRole
+  assume_role {
+		# Declaring a resource name. 
+    # The role_arn is the Amazon Resource Name (ARN) of the IAM Role to assume. 
+    # ARN is a unique number that is aligned to all the resources in the AWS account.
+    role_arn     = "arn:aws:iam::601452778024:role/serverless-jenkins-ecs-execution-role""
+    # Declaring a session name
+    session_name = "SESSION_NAME"
+  }
 }
